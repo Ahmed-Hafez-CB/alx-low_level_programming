8561247e623735ea6e93d275256f6566c149a489
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-17 13:52:29 -0700

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,18 @@
 
 /**
  * main - prints all alphabets twice
- * 	in upper and lowercase
+ *	in upper and lowercase
  *
  * Return: 0 Always
  */
 int main(void)
 {
-        char alphabet;
+	char alphabet;
 
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-                putchar(alphabet);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
