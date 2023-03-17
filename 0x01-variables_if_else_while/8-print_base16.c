54161f781a2f9905a8a4c2a6773e4e0f303d743b
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-17 14:33:59 -0700

    .

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16 characters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char alphabet;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	for (alphabet = 'a'; alphabet <= f; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
