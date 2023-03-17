d827b238c103bf2a5ad747f8b5482b1541958b82
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-17 14:14:57 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 0 to 9
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
