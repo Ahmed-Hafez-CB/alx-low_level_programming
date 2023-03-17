5440f2443e3db39a94dca5b1b8a488883bc9c3d6
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-17 16:22:06 -0700

    .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,16 @@ int main(void)
 {
 	int x;
 	int y;
-	int z;
 
 	for (x = 0; x < 10 ; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			z = x*10 + y;
-			if (x == y || z == (y*10 + x))
+			if (x == y || (x*10 + y) == (y*10 + x))
 				continue;
-			putchar(z + '0');
-			if (z == 89)
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x == 8 && y == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
