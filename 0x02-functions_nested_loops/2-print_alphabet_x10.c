f341f911e35f785801f46f21d88954f708610669
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 07:33:55 -0700

    .

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print alphabets x 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char alphabet;
+
+	i = 0;
+	while (i++ <= 9)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+		_putchar('\n');
+	}
+}
