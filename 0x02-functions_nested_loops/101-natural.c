e6210fccbc68507cb5394b9c53cb6cd4c8f20fe0
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 11:16:33 -0700

    .

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - all NN below 1024 multplies of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += 1;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
