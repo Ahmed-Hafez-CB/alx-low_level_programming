b65b25a659cbf4fb45823bebc8671880156ffe2d
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 11:08:41 -0700

    .

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table to a number
+ * @n: our number
+ */
+void print_times_table(int n)
+{
+	int num1;
+	int num2;
+	int res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			_putchar('0');
+			for (num2 = 1; num2 <= n; num2++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				res = num1 * num2;
+				if (res <= 99)
+					_putchar(' ');
+				if (res <= 9)
+					_putchar(' ');
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10)) % 10 + '0');
+				}
+				else if (res < 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
