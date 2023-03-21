1972c9848d2a9656ad88b048549eb71be5abfac2
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 13:14:26 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,7 +29,7 @@ void print_times_table(int n)
 					_putchar((res / 100) + '0');
 					_putchar(((res / 10)) % 10 + '0');
 				}
-				else if (res < 99 && res >= 10)
+				else if (res <= 99 && res >= 10)
 				{
 					_putchar((res / 10) + '0');
 				}
