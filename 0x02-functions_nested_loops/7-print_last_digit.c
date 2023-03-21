be9579a822f49fdbd77e45387c7dafd06ce29cd1
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 08:21:14 -0700

    .

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - get last digit of num
+ * @n: our int
+ *
+ * Return: last digit of n
+ */
+int print_last_digit(n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
+}
