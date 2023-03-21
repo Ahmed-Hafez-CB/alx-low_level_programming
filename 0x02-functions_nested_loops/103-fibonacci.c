309f835fd156137c2e2b5e3618e34a6e6d06662b
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-21 12:32:52 -0700

    .

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print even valued fibonacci
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long num1, num2, sum;
+	float total_sum;
+
+	num1 = 0;
+	num2 = 1;
+
+	while (1)
+	{
+		sum = num1 + num2;
+		if (sum > 4000000)
+			total_sum += sum;
+		num1 = num2;
+		num2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
