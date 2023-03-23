a550dbe24939f5d22433895546f2abebd894ac6d
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-23 16:56:42 -0700

    .

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	unsigned long int i,n;
+	unsigned long int i, n;
 
 	n = 612852475143;
 	for (i = 3; i < 782849; i += 2)
 		while ((n % i == 0) && (n != i))
 			n /= i;
-	printf("%lu\n",n);
+	printf("%lu\n", n);
 	return (0);
 }
