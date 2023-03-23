6340e6ff207f4fc66e39e5e0d128722af49bff6f
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-23 16:56:19 -0700

    .

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the prime factor
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i,n;
+
+	n = 612852475143;
+	for (i = 3; i < 782849; i += 2)
+		while ((n % i == 0) && (n != i))
+			n /= i;
+	printf("%lu\n",n);
+	return (0);
+}
