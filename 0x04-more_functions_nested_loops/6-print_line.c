c98aa4ca54a311ca5474a0ab4b02bea52acd0f9f
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-03-23 16:24:10 -0700

    .

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: our number
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
