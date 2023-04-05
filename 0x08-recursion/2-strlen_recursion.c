06adfb2f2cf3f6167205ee66ece866a6ff2fe207
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-04-05 02:03:13 -0700

    .

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print string length
+ * @s: our input
+ * Return: string lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return(0);
+}
