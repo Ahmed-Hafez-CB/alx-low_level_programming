be015f3c1a00cc2e1aeb23bab964d29e8edd2e75
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-04-05 03:05:57 -0700

    .

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calc sqrt of number
- * @n : our number
- * Return: sqrt of number
- */
-int _sqrt_recursion(int n)
-{
-	if (n == 0)
-		return (0);
-	return (valid(1, n));
-}
-/**
  * valid - check for sqrt
  * @a: first number
  * @b: second number
@@ -24,4 +13,16 @@ int valid(int a, int b)
 	else if (a * a > b)
 		return (-1);
 	return (valid(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - calc sqrt of number
+ * @n : our number
+ * Return: sqrt of number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (valid(1, n));
 }
