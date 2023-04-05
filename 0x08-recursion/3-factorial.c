c93493a028f0b335508348b17c64381152843c00
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-04-05 02:14:14 -0700

    .

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - calc the factorial
+ * @n: our number
+ * Return: the factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (n);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
