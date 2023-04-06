20ecc0ab338d572ab69af9b1e044570af9dcf82b
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-04-06 11:37:53 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints len(argv)
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
