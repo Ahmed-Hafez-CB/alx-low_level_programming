f010919385591bf043cc3ac00821560736297b17
Ahmed-Hafez-CB <ahmed.hafez.cb@gmail.com>
2023-05-05 17:15:23 -0700

    .

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - bit value
+ * @n: n
+ * @index: index
+ * Return: 1, -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = ~(1 << index);
+	*n = *n & i;
+
+	return (1);
+}
